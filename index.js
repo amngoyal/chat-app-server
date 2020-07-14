6fe79742672d2272225c4b80a65dd763b3af61b5
Aman Goyal <im.developer26@gmail.com>
2020-07-14 16:24:38 +0530

    connect disconnect event handle

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const socketio = require('socket.io');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("We have a new connection!!!");
+
+    socket.on('disconnect', () => {
+        console.log("user had left");
+    })
+})
+
+app.use(router);
 
 server.listen(PORT, () => { console.log(`Server is running at port ${PORT}`); })
